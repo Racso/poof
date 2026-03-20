57807cc8f27a5cf8ff7b8b8d492001f2a0087b86
Racso <correo@oscargomez.net>
2026-03-20 14:26:44 -0500

    Lowercase image name default (Docker requires lowercase)

--- server/handlers.go
+++ server/handlers.go
@@ -77,7 +77,7 @@ func (s *Server) createProject(w http.ResponseWriter, r *http.Request) {
 		req.Domain = req.Name + "." + s.cfg.Domain
 	}
 	if req.Image == "" {
-		req.Image = fmt.Sprintf("ghcr.io/%s/%s", s.cfg.GitHub.User, req.Name)
+		req.Image = fmt.Sprintf("ghcr.io/%s/%s", strings.ToLower(s.cfg.GitHub.User), req.Name)
 	}
 	if req.Repo == "" {
 		req.Repo = fmt.Sprintf("%s/%s", s.cfg.GitHub.User, req.Name)
